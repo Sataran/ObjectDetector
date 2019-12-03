75602413c923323adc2cd34b30df740d8d2e45c8
Przemek <p.sobol2012@gmail.com>
2019-12-03 16:51:57 +0100

    RealTimeDetectionActivity added

---
+++ app/src/main/java/inz/praca/przemyslaw/sobol/com/objectdetector/RealTimeDetectionActivity.java
@@ -0,0 +1,13 @@
+package inz.praca.przemyslaw.sobol.com.objectdetector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RealTimeDetectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real_time_detection);
+    }
+}
